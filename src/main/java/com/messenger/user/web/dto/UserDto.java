f5b4f2262a7d0080aa15ede61d8e2db48a232a4e
Mikhail <mihantoff@yandex.ru>
2025-02-07 15:54:32 +0300

    create user endpoint

--- src/main/java/com/messenger/user/web/dto/UserDto.java
+++ src/main/java/com/messenger/user/web/dto/UserDto.java
@@ -2,11 +2,15 @@ package com.messenger.user.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Builder
 @AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class UserDto {
 
     private String userName;
--- src/main/java/com/messenger/user/web/user/UserController.java
+++ src/main/java/com/messenger/user/web/user/UserController.java
@@ -4,18 +4,23 @@ import com.messenger.user.buissnes.UserService;
 import com.messenger.user.web.dto.CreateUserDto;
 import com.messenger.user.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/users")
+@RestController()
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/create")
-    public UserDto create (CreateUserDto user){
-
+    @PostMapping(value = "/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto create (@RequestBody CreateUserDto user){
         return userService.createUser(user);
     }
 }
